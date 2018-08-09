4b2c7a4e3bc84274d6cb4b81b2180f473a35e426
BoShurik <boshurik@gmail.com>
2018-08-09 14:20:55 +0300

    Add priority support to `automapper_plus.configurator` tag

--- DependencyInjection/Compiler/ConfigurationLoaderPass.php
+++ DependencyInjection/Compiler/ConfigurationLoaderPass.php
@@ -3,6 +3,7 @@
 namespace AutoMapperPlus\AutoMapperPlusBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
@@ -13,15 +14,17 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class ConfigurationLoaderPass implements CompilerPassInterface
 {
+    use PriorityTaggedServiceTrait;
+
     /**
      * @inheritdoc
      */
     public function process(ContainerBuilder $container)
     {
         $mapperFactory = $container->getDefinition('automapper_plus.mapper_factory');
-        $configurators = $container->findTaggedServiceIds('automapper_plus.configurator');
-        foreach ($configurators as $id => $_) {
-            $mapperFactory->addMethodCall('addConfigureCallback', [new Reference($id)]);
+        $configurators = $this->findAndSortTaggedServices('automapper_plus.configurator', $container);
+        foreach ($configurators as $configurator) {
+            $mapperFactory->addMethodCall('addConfigureCallback', [$configurator]);
         }
     }
 }
