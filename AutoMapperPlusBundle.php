f05848243e79325a7f116020f4ba4b1d8fb3986f
developermarshak <developer.marshak@gmail.com>
2024-04-08 17:44:07 +0300

    Added missing return type to AutoMapperPlusBundle::build() to fix deprecation

--- AutoMapperPlusBundle.php
+++ AutoMapperPlusBundle.php
@@ -16,7 +16,7 @@ class AutoMapperPlusBundle extends Bundle
     /**
      * @inheritdoc
      */
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new ConfigurationLoaderPass());
